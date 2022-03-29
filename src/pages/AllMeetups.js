b01a5ff43bf38394183383a942ccea015e00077e
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-03-29 16:17:39 +0100

    finished on the 'AllMeetups' component, now i am trying to outsource or create individual reusable components to handle smaller tasks

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,8 +1,31 @@
 import React from 'react'
 
 const AllMeetups = () => {
+
+    const DUMMY_DATA = [
+        {
+            id: "m1",
+            title: "This is a first meetup",
+            image: "./../../public/desk.png",
+            description: "This is a first, amazing meetup which you definitely should not miss. It will be fire"
+        }, {
+            id: "m2",
+            title: "This is a second meetup",
+            image: "./../../public/desk.png",
+            description: "This is a first, amazing meetup which you definitely should not miss. It will be fire"
+        }
+    ]
+
+
+
+
   return (
-    <div className='layout'>AllMeetups</div>
+    <div className='layout'>
+        <h1>All Meetups</h1>
+        <ul>
+            {DUMMY_DATA.map((meetup)=><li key={meetup.id}>{meetup.title}</li>)}
+        </ul>   
+    </div>
   )
 }
 
