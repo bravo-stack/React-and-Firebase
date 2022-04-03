9c1f6bb7549dc868f0fd9fe611aaa685044ac743
Bravo Stack <chukwuebukaonuh23@gmail.com>
2022-04-03 15:49:40 +0100

    added firebase real-time database

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,32 +1,63 @@
 import classes from "./NewMeetupForm.module.css"
 import Card from "../ui/Card"
+import { useRef } from "react"
 
-const NewMeetupForm = () => {
-  return (
-    <Card>
-        <form action="" className={classes.form}>
-            <div className={classes.control}>
-                <label htmlFor="title">Meetup Title</label>
-                <input type="text" name="title" id="title" required />
-            </div>  
-            <div className={classes.control}>
-                <label htmlFor="image">Meetup Image</label>
-                <input type="url" name="image" id="image" required />
-            </div>   
-            <div className={classes.control}>
-                <label htmlFor="address">Address</label>
-                <input type="text" name="address" id="address" required />
-            </div>
-            <div className={classes.control}>
-                <label htmlFor="description">Description</label>
-                <textarea name="description" id="description" required />
-            </div> 
-            <div className={classes.action}>
-                <button>Add Meetup</button>
-            </div>      
-        </form>
-    </Card>
-  )
+const NewMeetupForm = ({addMeetup}) => {
+    const titleInputRef = useRef()
+    const imageInputRef = useRef()
+    const addressInputRef = useRef()
+    const descriptionInputRef = useRef()
+
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+
+        const enteredTitle = titleInputRef.current.value
+        const enteredImage = imageInputRef.current.value
+        const enteredAddress = addressInputRef.current.value
+        const enteredDescription = descriptionInputRef.current.value
+
+        const newMeetupData = {
+            title: enteredTitle,
+            image: enteredImage,
+            address: enteredAddress,
+            description: enteredDescription
+        }
+
+        addMeetup(newMeetupData)
+
+        titleInputRef.current.value = ""
+        imageInputRef.current.value = ""
+        addressInputRef.current.value = ""
+        descriptionInputRef.current.value = ""
+    }
+
+
+    return (
+        <Card>
+            <form className={classes.form} onSubmit={submitHandler}>
+                <div className={classes.control}>
+                    <label htmlFor="title">Meetup Title</label>
+                    <input ref={titleInputRef} type="text" name="title" id="title" required />
+                </div>  
+                <div className={classes.control}>
+                    <label htmlFor="image">Meetup Image</label>
+                    <input ref={imageInputRef} type="url" name="image" id="image" required />
+                </div>   
+                <div className={classes.control}>
+                    <label htmlFor="address">Address</label>
+                    <input ref={addressInputRef} type="text" name="address" id="address" required />
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor="description">Description</label>
+                    <textarea ref={descriptionInputRef} rows={5} name="description" id="description" required />
+                </div> 
+                <div className={classes.actions}>
+                    <button>Add Meetup</button>
+                </div>      
+            </form>
+        </Card>
+    )
 }
 
 export default NewMeetupForm
--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -2,10 +2,22 @@ import React from 'react'
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 const NewMeetups = () => {
+
+  const addMeetupHandler = (meetupData) => {
+    fetch("https://react-router-project-56f43-default-rtdb.firebaseio.com/meetups.json", {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+  }
+
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm addMeetup={addMeetupHandler} />
     </section>
   )
 }
